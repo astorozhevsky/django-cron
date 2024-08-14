201b93907778d5bef229a9f457102176d3300239
Alexander Storozhevsky <astorozhevsky@ronasit.com>
2024-08-14 13:57:53 +0600

    fix:  Found wrong number (0) of indexes

--- django_cron/migrations/0004_alter_cronjoblog_options_and_more.py
+++ django_cron/migrations/0004_alter_cronjoblog_options_and_more.py
@@ -14,21 +14,21 @@ class Migration(migrations.Migration):
             name='cronjoblog',
             options={'get_latest_by': 'start_time'},
         ),
-        migrations.RenameIndex(
-            model_name='cronjoblog',
-            new_name='django_cron_code_966ed3_idx',
-            old_fields=('code', 'start_time'),
-        ),
-        migrations.RenameIndex(
-            model_name='cronjoblog',
-            new_name='django_cron_code_21f381_idx',
-            old_fields=('code', 'start_time', 'ran_at_time'),
-        ),
-        migrations.RenameIndex(
-            model_name='cronjoblog',
-            new_name='django_cron_code_89ad04_idx',
-            old_fields=('code', 'is_success', 'ran_at_time'),
-        ),
+        # migrations.RenameIndex(
+        #     model_name='cronjoblog',
+        #     new_name='django_cron_code_966ed3_idx',
+        #     old_fields=('code', 'start_time'),
+        # ),
+        # migrations.RenameIndex(
+        #     model_name='cronjoblog',
+        #     new_name='django_cron_code_21f381_idx',
+        #     old_fields=('code', 'start_time', 'ran_at_time'),
+        # ),
+        # migrations.RenameIndex(
+        #     model_name='cronjoblog',
+        #     new_name='django_cron_code_89ad04_idx',
+        #     old_fields=('code', 'is_success', 'ran_at_time'),
+        # ),
         migrations.AlterField(
             model_name='cronjoblog',
             name='code',
